108692a1e57ad701a53563b05216c9afeac51218
pauldrinn <tahsinemirhan58771@gmail.com>
2023-10-13 02:09:29 -0700

    Upgraded to Cement 3

--- ePEST.py
+++ ePEST.py
@@ -16,6 +16,7 @@ the distribution).
 import time
 import signal
 
+from cement import App, init_defaults
 from cement.core import backend
 from cement.core import foundation
 from cement.core import hook
@@ -24,10 +25,7 @@ from cement.core import exc
 from context.env import env_checking
 from context.opt import opt_validating
 
-import bll.core as core
 from bll.exo import ExoBlocker
-import bll.dedup as dedup
-import bll.param as param
 
 import multiprocessing
 import bll.workbee as workbee
@@ -35,7 +33,7 @@ import bll.workbee as workbee
 #=============================================================================
 #  Set default config options
 #=============================================================================
-defaults = backend.defaults("exoApp", "log")
+defaults = init_defaults("exoApp", "log")
 defaults['exoApp']['debug'] = False
 timelabel = time.strftime("%Y-%m-%d_%H:%M:%S", time.gmtime())
 defaults['log']['file'] = 'ePEST_' + timelabel + '.log' ##
@@ -43,7 +41,7 @@ defaults['log']['file'] = 'ePEST_' + timelabel + '.log' ##
 #=============================================================================
 #  Create an application
 #=============================================================================
-app = foundation.CementApp("exoApp", config_defaults=defaults)
+app = App("exoApp", config_defaults=defaults)
 
 #=============================================================================
 #  Register any framework hook functions after app creation, and before
@@ -51,33 +49,33 @@ app = foundation.CementApp("exoApp", config_defaults=defaults)
 #=============================================================================
 def my_pre_setup_hook(app):
     #=========================================================================
-	#  In this stage, we would like to check the environment.
+    #  In this stage, we would like to check the environment.
     #  note: app.log is still not available at this time.
-	#=========================================================================
+    #=========================================================================
     env_checking(app)
 
 def my_post_setup_hook(app):
     #=========================================================================
-	#  In this stage, we would like to decorate the application logger.
+    #  In this stage, we would like to decorate the application logger.
     #  note: the app.Logger is available, but app.pargs is not available
-	#=========================================================================
+    #=========================================================================
     pass
     
 def my_pre_run_hook(app): 
     #=========================================================================
-	#  In this stage, app.pargs is still not available due to app.argv has 
+    #  In this stage, app.pargs is still not available due to app.argv has 
     #  not been parsed.
-	#========================================================================= 
+    #========================================================================= 
     pass
   
 def my_post_run_hook(app):
     #=========================================================================
-	#  In this stage, all cement-framework related configuration has finished,
+    #  In this stage, all cement-framework related configuration has finished,
     #  and the app.pargs also available now. We can do options-validation in 
     #  regarding to our business logics.
-	#=========================================================================
+    #=========================================================================
     opt_validating(app)
-    
+
 hook.register('pre_setup', my_pre_setup_hook)
 hook.register('post_setup', my_post_setup_hook)
 hook.register('pre_run', my_pre_run_hook)
@@ -87,33 +85,33 @@ try:
     app.setup()
 
     app.args.description = "ePEST: ChIP-exo paired-end sequencing processing toolkit, for peak-calling and border identify."
-    
+
     app.args.add_argument('input', metavar='--input.bam', help="the bamfile from ChIP-exo. REQUIRED")
-    
+
     app.args.add_argument('-o', '--output', help="the output folder for results. \
                                                   Default is the current directory.")
-        
+
     #app.args.add_argument('-mode', '--mode', choices=['single', 'pair'], default='pair',
     #                      help="The sequencing mode. Default=pair")
-    
-    app.args.add_argument('-C', '--include-chroms', action='store', metavar='chroms', type=str, nargs='*', 
+
+    app.args.add_argument('-C', '--include-chroms', action='store', metavar='chroms', type=str, nargs='*',
                           default=None, dest='included_chroms', help="the chromosomes to be included for analysis. Default=None")
 
     app.args.add_argument('-r', '--regex', action='store', metavar='REGEX', \
                            default="[a-zA-Z0-9]+:[0-9]:([0-9]+):([0-9]+):([0-9]+).*",
                            help="the read id pattern. The Default is setted with Solexa platform: [a-zA-Z0-9]+:[0-9]:([0-9]+):([0-9]+):([0-9]+).*")
-                          
+
     app.args.add_argument('-p', '--pvalue', action='store', metavar='float', type=float, default=1e-5,
                           help="the p value of statistical significant for trigging peak-calling. Default=1e-5.")
-                 
+
     app.args.add_argument('-D', '--dedup', action='store', choices=['True', 'False'], default=False,
-                          help="the flag for removing those duplicated reads; only works for paired sequencing. Default=True.")    
-    
+                          help="the flag for removing those duplicated reads; only works for paired sequencing. Default=True.")
+
     app.args.add_argument('-d', '--dist', action='store', type=int, metavar='int', default=100,
                           help="the cut-off distance between reads touched on the flow chip for optical duplicate detection. Default=100.")
-    
+
     app.args.add_argument('-q', '--qvalue', action='store', type=float, metavar='float', default=0.001,
-                          help="the pvalue used for amplification fragments detection. Default=0.001.") 
+                          help="the pvalue used for amplification fragments detection. Default=0.001.")
 
     app.args.add_argument('-R', '--rscan', action='store', type=int, metavar='int', default=20,
                           help="the r-scan parameter, a minimal number of R. Default=20.")
@@ -128,62 +126,62 @@ try:
                           help="the probability of chernoff inequality. Default=0.05.")
 
     app.args.add_argument('-k', '--outlier', action='store', type=float, metavar='float', default=2.0,
-                          help="the kth-fold std deviation for outlier detection. Default=2.0.")                          
+                          help="the kth-fold std deviation for outlier detection. Default=2.0.")
     app.args.add_argument('-t', '--threads', action='store', metavar='int', type=int, default=2,
                           help="the number of threads enabled for parallel computation.")
-                          
-                          
-    ##                       
+
+
+    ##
     app.run()
     #=========================================================================
     # after this run() called, the opt_validating() will execute before going
     # into detailed business logic layer
     #=========================================================================
-    
+
     #app.log.info("%s: Checked good! Starting..." % (time.ctime()) )
     app.log.info("Checked good! Starting..." )
-    
+
     #=========================================================================
     # now we can go to our specific application functionality
     # prepare for our application business (business logic layer: bll)
     #=========================================================================
-    
+
     #=========================================================================
     # may assign the exoBlocker to a member of app, for extension in future
     #=========================================================================
     exoBlocker = ExoBlocker(app) 
     print(app.pargs)
-    #'''
+
     chromNodes = multiprocessing.JoinableQueue()
     results = multiprocessing.Queue()
     # Start consumers
     t = app.pargs.threads # the number of cores ##
     pargs = app.pargs
     plog = app.log
-    consumers = [ multiprocessing.Process(target=workbee.processWorkBee, args=(chromNodes, results, pargs, plog)) for i in range(t) ]
+    consumers = [multiprocessing.Process(target=workbee.processWorkBee, args=(chromNodes, results, pargs, plog)) for i in range(t)]
     for w in consumers:
         w.start()
-    
+
     # Enqueue jobs
     baseTree = exoBlocker.dataModel.baseTree
     treeNodes = baseTree.all_nodes()
-    branches = [ node for node in treeNodes if baseTree.level(node.identifier)==1 ]    
+    branches = [node for node in treeNodes if baseTree.level(node.identifier)==1]
     for node in branches:
         chromNodes.put(node)
-        
+
     # Add a poison pill for each consumer
     for i in range(0, t):
         chromNodes.put(None)
     # Wait for all of the nodes to finish
     chromNodes.join()
-    
+
     # Start printing results
     num_jobs = len(branches)
     while num_jobs:
         result = results.get()
         print('Result:', result)
-        num_jobs -= 1  
-    #'''      
+        num_jobs -= 1
+
 except exc.CaughtSignal as e:
     if e.signum == signal.SIGTERM:
         print("Caught signal SIGTERM...")
